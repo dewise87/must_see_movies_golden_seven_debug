fa03bc84c5e895c397ff58de0e6e569a359218f1
dewise87 <daniel.wise@chicagobooth.edu>
2015-10-29 17:47:17 -0500

    HW5: Debugging

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
   # Routes for the Director resource:
   # CREATE
   get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
-  get('/create_director', { :controller => 'director', :action => 'create_row' })
-
-  # READ
-  get('/directors', { :controller => 'directors', :action => 'index' })
-  get('/directors/id', { :controller => 'directors', :action => 'show' })
+  get('/create_director', { :controller => 'directors', :action => 'create_row' })
 
   # UPDATE
   get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
@@ -17,6 +13,10 @@ Rails.application.routes.draw do
 
   # DELETE
   get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
+
+  # READ
+  get('/directors', { :controller => 'directors', :action => 'index' })
+  get('/directors/:id', { :controller => 'directors', :action => 'show' })
   #------------------------------
 
   # Routes for the Movie resource:
@@ -24,16 +24,16 @@ Rails.application.routes.draw do
   get('/movies/new_form', { :controller => 'movies', :action => 'new_form' })
   get('/create_movie', { :controller => 'movies', :action => 'create_row' })
 
-  # READ
-  get('/movies', { :controller => 'movies', :action => 'index' })
-  get('/movies/:id', { :controller => 'movies', :action => 'show' })
-
   # UPDATE
   get('/movies/:id/edit_form', { :controller => 'movies', :action => 'edit_form' })
   get('/update_movie/:id', { :controller => 'movies', :action => 'update_row' })
 
   # DELETE
   get('/delete_movie/:id', { :controller => 'movies', :action => 'destroy' })
+
+  # READ
+  get('/movies', { :controller => 'movies', :action => 'index' })
+  get('/movies/:id', { :controller => 'movies', :action => 'show' })
   #------------------------------
 
   # Routes for the Role resource:
@@ -41,16 +41,16 @@ Rails.application.routes.draw do
   get('/roles/new_form', { :controller => 'roles', :action => 'new_form' })
   get('/create_role', { :controller => 'roles', :action => 'create_row' })
 
-  # READ
-  get('/roles', { :controller => 'roles', :action => 'index' })
-  get('/roles/:id', { :controller => 'roles', :action => 'show' })
-
   # UPDATE
   get('/roles/:id/edit_form', { :controller => 'roles', :action => 'edit_form' })
   get('/update_role/:id', { :controller => 'roles', :action => 'update_row' })
 
   # DELETE
   get('/delete_role/:id', { :controller => 'roles', :action => 'destroy' })
+
+  # READ
+  get('/roles', { :controller => 'roles', :action => 'index' })
+  get('/roles/:id', { :controller => 'roles', :action => 'show' })
   #------------------------------
 
   # Routes for the Actor resource:
@@ -58,15 +58,15 @@ Rails.application.routes.draw do
   get('/actors/new_form', { :controller => 'actors', :action => 'new_form' })
   get('/create_actor', { :controller => 'actors', :action => 'create_row' })
 
-  # READ
-  get('/actors', { :controller => 'actors', :action => 'index' })
-  get('/actors/:id', { :controller => 'actors', :action => 'show' })
-
   # UPDATE
   get('/actors/:id/edit_form', { :controller => 'actors', :action => 'edit_form' })
   get('/update_actor/:id', { :controller => 'actors', :action => 'update_row' })
 
   # DELETE
   get('/delete_actor/:id', { :controller => 'actors', :action => 'destroy' })
+
+  # READ
+  get('/actors', { :controller => 'actors', :action => 'index' })
+  get('/actors/:id', { :controller => 'actors', :action => 'show' })
   #------------------------------
 end
